99878c7d3aa754849146822bcd9cf244bf6d9327
Jimena Hernández <her21199@uvg.edu.gt>
2024-01-15 12:46:47 -0600

    Update ProjectItem.jsx

--- components/ProjectItem/ProjectItem.jsx
+++ components/ProjectItem/ProjectItem.jsx
@@ -11,10 +11,10 @@ const ProjectItem = ({ project }) => {
     };
 
     return (
-        <div className="glassmorphism lg:w-1/4 m-2 h-96">
+        <div className="glassmorphism lg:w-1/4 m-2 h-96 relative">
             <div className="flex flex-col items-center justify-between">
                 <img src={project.image} alt={project.title} className="w-full h-60 rounded-lg object-cover" />
-                <div className="flex flex-col items-center justify-center">
+                <div className="flex flex-col items-center justify-center ">
                     <h1 className="font-inter text-xl lg:text-2xl font-bold text-center mt-4 mb-2">{project.title}</h1>
                     <FaArrowAltCircleRight className="text-4xl text-kaqui cursor-pointer absolute bottom-4 right-2" onClick={handleMoreClick} />
                 </div>
