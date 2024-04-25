d5f693acd238a1dd60725165787c6ec6cefb08ab
Jimena Hernández <her21199@uvg.edu.gt>
2024-04-25 13:59:31 -0600

    Update page.jsx

--- app/blog/[postId]/page.jsx
+++ app/blog/[postId]/page.jsx
@@ -2,6 +2,9 @@
 import React, { useState, useEffect } from 'react'
 import NavBarSecond from '@/components/NavBarSecond/NavBarSecond'
 import PocketBase from 'pocketbase'
+import { FaLinkedinIn } from "react-icons/fa"
+
+
 
 const BlogPage = ({params}) => {
     const [post , setPost] = useState('')
@@ -21,6 +24,13 @@ const BlogPage = ({params}) => {
         fetchData()
     }, [params.postId])
 
+    const createLinkedInShareLink = (url, title) => {
+        return `https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(url)}&title=${encodeURIComponent(title)}`;
+    }
+
+    const postUrl = `https://www.proinmo502.com/blog/${params.postId}`;
+
+
   return (
     <div className='flex flex-col'>
       <NavBarSecond />
@@ -28,6 +38,14 @@ const BlogPage = ({params}) => {
             <div className="flex flex-col w-full h-full mx-auto pt-20 lg:pt-40 lg:px-10 px-8 ">
                 <h1 className="text-lg lg:text-xl font-bold font-montserrat text-start lg:text-center py-4 m-0">{post.title}</h1>
                 <div className="text-grayish text-sm lg:text-base font-montserrat blog-content"  dangerouslySetInnerHTML={{ __html: post.post }}></div>
+                <div className="flex py-4 justify-end w-full">
+                    <div className='flex flex-col gap-2'>
+                        <span className='text-grayish text-sm lg:text-base font-montserrat'>Compartir en</span>
+                        <a href={createLinkedInShareLink(postUrl,post.title)} target="_blank" rel="noopener noreferrer" className='flex justify-end'>
+                            <FaLinkedinIn className='text-kaqui text-lg'></FaLinkedinIn>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
